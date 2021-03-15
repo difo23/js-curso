5054f2d85796bd36d3ad940dc69b7bd863bce112
Lizandro <lizandro.ramirez@ucateci.edu.do>
2021-03-15 14:41:52 -0400

    Codigo del temario variables.js

---
+++ src/resumen-js/variables.js
@@ -0,0 +1,22 @@
+
+// Tema 1: Variables 
+
+// Comentarios Cortos
+/*Inicio bloque de comentarios
+    Comentarios largos:
+    Puedo colocar descripciones de mi codigo. 
+
+    Cierro bloques de comentarios con: 
+*/
+
+// Tipos de variables en JS: var, let y const
+// Declaracion de la variable
+var nombre = ' Juan ';// Tipo String
+
+// Uso de la variable: nombre
+
+// Asiganacion destructiva
+nombre = 8 // Tipo number
+
+//Snippets clg
+console.log(nombre);
